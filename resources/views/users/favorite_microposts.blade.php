9c281d49c6f92174d1baf1efcabe03f2bcbdd36d
k.k <kaz.training2020@gmail.com>
2020-09-11 11:42:44 +0000

    kondokoso

--- resources/views/users/favorite_microposts.blade.php
+++ resources/views/users/favorite_microposts.blade.php
@@ -6,10 +6,16 @@
             <img class="mr-2 rounded" src="{{ Gravatar::get($user->email,['size' => 50]) }}" alt="">
             <div class="media-body mb-2">
                 <div>
-                    <p class="mb-1">{!! link_to_route('users.show',$favorite->user->name,['user' => $favorite->user->id]) !!}<span class=" pl-2 text-muted">posted at {{ $favorite->created_at }}</span></p>
+                    <p class="mb-1">{!! link_to_route('users.show',$favorite->user->name,['user' => $favorite->user->id]) !!}
+                    <span class=" pl-2 text-muted">posted at {{ $favorite->created_at }}</span></p>
+                </div>
                 <div>
                     {{$favorite->content}}
                 </div>
+                <div class="d-inline-block">
+                    @include('user_favorite.favorite_button',['micropost' => $favorite])
+                </div>
+
             </div>
         </li>
     @endforeach
